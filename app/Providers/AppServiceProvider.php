e30bc60bcbc98c80dcd2f9441d18dfec2cbf3ce2
Jhaseft <jhasesaat@gmail.com>
2025-11-27 18:49:11 -0400

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Forzar URLs con HTTPS en producción
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
+        // Tu configuración de Vite (prefetch)
         Vite::prefetch(concurrency: 3);
     }
 }
